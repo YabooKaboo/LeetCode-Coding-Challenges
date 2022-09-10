ffe257bb37c34125b26d6a29163c2a94f2c570a4
YabooKaboo <yide_cai@yahoo.com>
2022-09-09 21:07:16 -0500

    Update romanToInt.js

--- romanToInt.js
+++ romanToInt.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let map={I:1,V:5,X:10,L:50,C:100,D:500,M:1000}
-    let sum=0
+    let map={I:1,V:5,X:10,L:50,C:100,D:500,M:1000};
+    let sum=0;
     for(let i=0;i<s.length-1;i++){
-        map[s[i]]<map[s[i+1]]?sum-=map[s[i]]:sum+=map[s[i]]
+        map[s[i]]<map[s[i+1]]?sum-=map[s[i]]:sum+=map[s[i]];
     }
-    return sum+map[s[s.length-1]]
+    return sum+map[s[s.length-1]];
 };
